a586218cd3abc2681c66db6f02098e9a30442a0d
Tobias Gies <tobias@tobiasgies.de>
2024-10-09 19:55:44 +0200

    Update resolvconf settings

--- setup.sh
+++ setup.sh
@@ -55,7 +55,8 @@ rm /etc/resolv.conf
 cat > /etc/resolv.conf <<EOF
 nameserver 127.0.0.1
 nameserver 1.1.1.1
-nameserver 1.0.0.1
+nameserver 9.9.9.9
+nameserver 192.168.10.1
 EOF
 sed -i -E -e 's/#?DNSStubListener=yes/DNSStubListener=no/' \
     /etc/systemd/resolved.conf
